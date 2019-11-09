a4975da11c88a575db02b5585e7589d90a28c624
Tyler <t.charlantini@gmail.com>
2019-11-09 05:54:04 -0500

    Setup of management API. Added support for adding user submissions to database and querying previous submissions for their subscriptions.

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+var bodyParser = require("body-parser");
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use(bodyParser.json());
+
+
+
+app.all("/addphonenumber", addPhoneNumber);
+function addPhoneNumber(req, res) {
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("db");
+		var myobj = req.body;
+		myobj.keywords = myobj.keywords.split(',');
+		dbo.collection("users").update({"phone": myobj.phone}, myobj, {upsert: true})
+		})
+	console.log("1 document inserted/updated");
+	res.status(200).send("Success!");
+}
+
+app.all("/subscriptions", checkSubscriptions);
+function checkSubscriptions(req, res) {
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("db");
+		var myobj = req.body.phone;
+		dbo.collection("users").findOne({"phone": myobj}, function(err, result) {
+			if(err) throw err;
+			console.log(result.keywords);
+			db.close();
+		})
+	})
+	res.status(200).send("Success!");
+}
+
+app.listen(8000);
